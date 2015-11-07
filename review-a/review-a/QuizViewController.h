d06a370c471ac01aa6d4c9ed99250b30661b48ec
another-false-shadow <another.false.shadow@gmail.com>
2015-11-07 11:11:36 -0800

    new files
    added QuizViewController

---
+++ review-a/review-a/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  review-a
+//
+//  Created by Dark Realm Studios+ on 2015-11-07.
+//  Copyright (c) 2015 Dark Realm Studios+. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
---
+++ review-a/review-a/QuizViewController.m
@@ -0,0 +1,37 @@
+//
+//  QuizViewController.m
+//  review-a
+//
+//  Created by Dark Realm Studios+ on 2015-11-07.
+//  Copyright (c) 2015 Dark Realm Studios+. All rights reserved.
+//
+
+#import "QuizViewController.h"
+
+@interface QuizViewController ()
+
+@end
+
+@implementation QuizViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
